fb266c3a5f12c28356d97e162082c28418b71874
Дима <dima.aldakimov@yandex.ru>
2021-08-18 00:00:39 +0300

    add img

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     else
     	document.documentElement.setAttribute("data-theme", "light");
     var themeSwitcher = document.getElementById("theme-switcher");
-    themeSwitcher.innerHTML = currentTheme === "dark" ? '<svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2m0 18v2M4.22 4.22l1.42 1.42m12.72 12.72 1.42 1.42M1 12h2m18 0h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"/></svg>' : '<svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>'
+    themeSwitcher.innerHTML = currentTheme === "dark" ? '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><title>sun</title><path d="M12,1V3m0,18v2M4.22,4.22,5.64,5.64M18.36,18.36l1.42,1.42M1,12H3m18,0h2M4.22,19.78l1.42-1.42M18.36,5.64l1.42-1.42"/><circle cx="12" cy="12" r="5"/></svg>' : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.96 19.96" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><title>moon</title><path d="M19,10.79A9,9,0,1,1,9.17,1,7,7,0,0,0,19,10.79Z" transform="translate(0)"/></svg>'
     themeSwitcher.onclick = function() {
       	var currentTheme = document.documentElement.getAttribute("data-theme");
       	var switchToTheme = currentTheme === "dark" ? "light" : "dark"
-      	themeSwitcher.innerHTML = switchToTheme === "dark" ? '<svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2m0 18v2M4.22 4.22l1.42 1.42m12.72 12.72 1.42 1.42M1 12h2m18 0h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"/></svg>' : '<svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>'
-      	document.documentElement.setAttribute("data-theme", switchToTheme);
+      	themeSwitcher.innerHTML = switchToTheme === "dark" ? '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><title>sun</title><path d="M12,1V3m0,18v2M4.22,4.22,5.64,5.64M18.36,18.36l1.42,1.42M1,12H3m18,0h2M4.22,19.78l1.42-1.42M18.36,5.64l1.42-1.42"/><circle cx="12" cy="12" r="5"/></svg>' : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.96 19.96" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><title>moon</title><path d="M19,10.79A9,9,0,1,1,9.17,1,7,7,0,0,0,19,10.79Z" transform="translate(0)"/></svg>'
+        document.documentElement.setAttribute("data-theme", switchToTheme);
       	localStorage.setItem('theme', switchToTheme);
     }
 });
